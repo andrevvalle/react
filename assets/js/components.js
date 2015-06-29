f6b9de07c4626229d82d254ab9b836bea0686159
Andre Valle <andrevvalle@icloud.com>
2015-06-29 17:40:23 -0300

    update

--- assets/js/components.js
+++ assets/js/components.js
@@ -2,48 +2,86 @@
 
 (function(Reflux, Namespace, global){
 	
-	var App = React.createClass({
+	// var App = React.createClass({
 		
-		mixins: [Reflux.connect(Namespace.store)],
-		getInitialState() {
-			return {
-				name: "",
-				age: 0
-			}
-		},
-		handleSubmit(event) {
-			event.preventDefault();
-			var age = this.state.age
+	// 	mixins: [Reflux.connect(Namespace.store)],
+	// 	getInitialState() {
+	// 		return {
+	// 			name: "",
+	// 			age: 0
+	// 		}
+	// 	},
+	// 	handleSubmit(event) {
+	// 		event.preventDefault();
+	// 		var age = this.state.age
 			
-			Namespace.store.onUpdateAge(age);
-		},
-		update(e) {
-			this.setState({age: e.target.value});
-		},
-		render() {
-			return (<FormRen 
-						name={this.state.person.name}
-						age={this.state.person.age}
-						change={this.update}
-						submit={this.handleSubmit} />);
-		}
+	// 		Namespace.store.onUpdateAge(age);
+	// 	},
+	// 	update(e) {
+	// 		this.setState({age: e.target.value});
+	// 	},
+	// 	render() {
+	// 		return (<FormRen 
+	// 					name={this.state.person.name}
+	// 					age={this.state.person.age}
+	// 					change={this.update}
+	// 					submit={this.handleSubmit} />);
+	// 	}
 
+	// });
+
+	// var FormRen = React.createClass({
+	// 	render() {
+	// 		return (
+	// 			<div>
+	// 				<p>Name: {this.props.name}</p>
+	// 				<p>Age: {this.props.age}</p>
+	// 				<form className="form-inline" onSubmit={this.props.submit}>
+	// 					<input type="number" className="form-control" ref="number" onChange={this.props.change} />
+	// 					<input type="submit" className="btn" value="Update Age"/>
+	// 				</form>
+	// 			</div>);
+	// 	}
+	// });
+
+	// React.render(<App />, document.getElementById('main'));
+
+	var Button = React.createClass({
+	  getInitialState:function(){
+	    return {val: 0}
+	  },
+	  update: function(){
+	    this.setState({val: this.state.val+10});
+	  },
+	  render:function(){
+	    return <button onClick={this.update}>{this.state.val}</button>
+	  },
+	  componentDidMount:function(){
+	    console.log('mounted');
+	  },
+	  componentWillUnmount:function(){
+	    console.log('bye!');
+	  },
 	});
 
-	var FormRen = React.createClass({
-		render() {
-			return (
-				<div>
-					<p>Name: {this.props.name}</p>
-					<p>Age: {this.props.age}</p>
-					<form className="form-inline" onSubmit={this.props.submit}>
-						<input type="number" className="form-control" ref="number" onChange={this.props.change} />
-						<input type="submit" className="btn" value="Update Age"/>
-					</form>
-				</div>);
-		}
+	var App = React.createClass({
+	  mount: function(){
+	    React.render(<Button />, document.getElementById('app'));
+	  },
+	  unmount: function(){
+	    React.unmountComponentAtNode(document.getElementById('app'));
+	  },
+	  render:function(){
+	    return (
+	      <div>
+	        <button onClick={this.mount}>Mount</button>
+	        <button onClick={this.unmount}>Unmount</button>
+	        <div id="app"></div>
+	      </div>
+	      );
+	  }
 	});
 
-	React.render(<App />, document.getElementById('main'));
+	React.render(<App />, document.body);
 
 })(window.Reflux, window.Oi, window);
