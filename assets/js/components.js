4f6ce1b85cd21eb120e9f8a38684894d06acff0c
Andre Valle <andrevvalle@icloud.com>
2015-06-29 17:44:09 -0300

    component lifeCycle

--- assets/js/components.js
+++ assets/js/components.js
@@ -51,9 +51,13 @@
 	    return {val: 0}
 	  },
 	  update: function(){
-	    this.setState({val: this.state.val+10});
+	    this.setState({val: this.state.val+1});
+	  },
+	  componentWillMount:function(){
+	    console.log('mounting');
 	  },
 	  render:function(){
+	    console.log('rendering');
 	    return <button onClick={this.update}>{this.state.val}</button>
 	  },
 	  componentDidMount:function(){
